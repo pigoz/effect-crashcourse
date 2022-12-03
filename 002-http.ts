12fe24e16d9f3a5ad70be51312445ba5d9c81f82
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2022-12-03 17:19:16 +0100

    http: add a few comments

--- 002-http.ts
+++ 002-http.ts
@@ -1,22 +1,36 @@
 import { pipe } from "@fp-ts/data/Function";
 import * as Z from "@effect/io/Effect";
 
+/*
+ * The most iconic asynchronous example in JavaScript is fetching from APIs.
+ * In this example we build a small program to fetch a Gist.
+ */
 const fetchGist = (id: string) =>
   Z.tryCatchPromise(
     () => fetch(`https://api.github.com/gists/${id}`),
-    () => "fetch error" as const
+    () => "fetch err" as const
   );
 
 const getJson = (res: Response) =>
   Z.tryCatchPromise(
-    () => res.json() as Promise<unknown>,
-    () => "get json error" as const
+    () => res.json() as Promise<unknown>, // Promise<any> otherwise
+    () => "get json err" as const
   );
 
+const print = (message: string) =>
+  Z.flatMap((obj) =>
+    Z.sync(() => {
+      console.log(message, obj);
+      return obj;
+    })
+  );
+
+const id = "97459c0045f373f4eaf126998d8f65dc";
+
 const program = pipe(
-  fetchGist("97459c0045f373f4eaf126998d8f65dc"),
-  Z.flatMap(getJson),
-  Z.flatMap((gist) => Z.sync(() => console.log(gist)))
+  fetchGist(id), // Z.Effect<never, 'fetch error', Response>
+  Z.flatMap(getJson), // Z.Effect<never, 'fetch err' | 'get json err', unknown>
+  print("gist") // Z.Effect<never, 'fetch err' | 'get json err', void>
 );
 
 Z.unsafeRunPromiseExit(program);
