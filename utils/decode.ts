32733eb1bccc8c313a5b8d64abaaf75714359121
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2023-02-12 08:42:26 +0100

    feat(decode): add effect wrapper

--- utils/decode.ts
+++ utils/decode.ts
@@ -1,3 +1,4 @@
+import * as Z from "@effect/io/Effect";
 import * as S from "@fp-ts/schema";
 import * as E from "@fp-ts/core/Either";
 import { pipe } from "@fp-ts/core/Function";
@@ -17,4 +18,8 @@ export function decode<A>(schema: S.Schema<A>) {
       }),
       E.mapLeft(_ => new DecodeError(formatErrors(_))),
     );
+}
+
+export function decodee<A>(schema: S.Schema<A>) {
+  return (input: unknown) => Z.absolve(Z.sync(() => decode(schema)(input)));
 }
