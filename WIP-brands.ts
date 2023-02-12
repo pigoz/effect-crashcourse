71441e97715196220b89e028bd97ae667743a985
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2023-02-12 08:42:42 +0100

    feat(brand): add initial example

---
+++ WIP-brands.ts
@@ -0,0 +1,10 @@
+import * as Brand from "@effect/data/Brand";
+
+export type Eur = number & Brand.Brand<"Eur">;
+export const Eur = Brand.nominal<Eur>();
+
+export type Payed = number & Brand.Brand<"Payed">;
+export const Payed = Brand.nominal<Payed>();
+
+const payment = Payed(Eur(10));
+console.log("euros: %o", payment);
